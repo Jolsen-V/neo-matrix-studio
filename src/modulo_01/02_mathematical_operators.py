2bb35094087ce581fbceb07651380b019422f553
Jolsen-V <jvidalmilla03@gmail.com>
2026-05-13 21:52:28 -0500

    feat: modulo 01 - print, operadores matematicos y comparacion

---
+++ src/modulo_01/02_mathematical_operators.py
@@ -0,0 +1,19 @@
+#---- MATHEMATICAL OPERATORS ----
+print(10 + 5)  # Addition 
+print(10 - 5)  # Subtraction
+print(10 * 5)  # Multiplication
+print(10 / 5)  # Division
+print(10 % 3)  # Modulus (rest "1")
+print(10 ** 2) # Exponentiation
+print(10 // 3) # Floor Division (integer division "3" ) 
+
+
+print("-----------------------------------------------------------------------------------------")
+
+#combination of mathematical ideas 
+print(340+238+(43-11)-23)
+print(-324+32 -(90/4)+ 25**4)
+print(82*5 + 69 -21*34*4 + 2**3)
+print(450//5 +9283 - 8493/0.25 )
+print((410%4)+ (938%7))
+print(89//4 + 24/9 )
