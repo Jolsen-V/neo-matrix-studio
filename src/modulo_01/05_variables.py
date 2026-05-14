4a0db5c5bb41e76f2bae12d99a6e7866672858ea
Jolsen-V <jvidalmilla03@gmail.com>
2026-05-14 16:53:40 -0500

    feat: modulo 01 - input de usuario completado

---
+++ src/modulo_01/05_variables.py
@@ -0,0 +1,44 @@
+# Module 03: lesson 05
+
+#-------- variables ---------
+# topic: variables 
+
+# *** text : "string(str)"
+name = "Claudio"
+age = "30"
+
+
+#* number : "integer(int)"
+day = 20
+price = 200000
+
+
+#* decimal : "float"
+temperature = 36.5
+python = 3.12
+
+
+#* Boolean: True or False
+passive = False
+active = True
+
+
+print("***********************************************************************")
+
+# check data types
+print(type(name))  #  'str'
+print(type(age))   # 'str'
+print(type(day))   # 'int'
+print(type(price)) # int'
+print(type(temperature)) # 'float'
+print(type(python)) #'float'
+print(type(passive)) # 'bool'
+print(type(active))  # 'bool'
+
+
+#* example of combination of variables
+print("my name is", name, "I have", age, "years old")
+print("today is ", day, "for buy a new car and price is $:", price)
+print("the temperature of today is", temperature, "the version of python is", python)
+print("what is earn is passive?: ", passive, "what is earn is active?: ", active)
+
---
+++ src/modulo_01/06_input.py
@@ -0,0 +1,24 @@
+# Module 01: lesson 06
+
+#======== input function ========
+# topic: input function
+
+#* input(): always returns text (string)
+name = input("what is your name? ")
+city = input("where are you from? ")
+
+age = int(input("what is your age? "))
+pi = float(input("what is the value of pi? "))
+temperature = float(input("what is the temperature of today? "))
+
+salary = float(input("how much do you earn? "))
+
+
+# example of input()
+#* presentation of the data
+
+print("my name is", name, "nice to meet you, I am from",city)
+print("I have", age, "years old and i like the value of pi :",pi)
+print("the climate of today is", temperature)
+print(" I earn $:", salary)
+print("thank you for your presentation")
