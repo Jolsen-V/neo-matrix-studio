ca6a0bfc93ab1bef1b38742ae7b4a26314e1d8ce
Jolsen-V <jvidalmilla03@gmail.com>
2026-05-16 18:08:10 -0500

    feat: modulo 01 - proyecto 01 calculadora ROI startup

---
+++ src/modulo_01/real_projects/01_project_roi_startup.py
@@ -0,0 +1,37 @@
+# Module 01: project 01
+
+#name: project 01 - calculate ROI of a startup
+
+print("=========================================")
+print(" 🌱 ROI calculate - first startup project ⭐️ ")
+print("=========================================")
+
+# ask for data
+investment = float(input("How much do you want to invest: $."))
+revenue = float(input("Revenue obtained of the investment: $."))
+investment_time = int(input("How many years do you want to invest?:"))
+investment_months = int(input("How many months do you want to invest?:"))
+
+# calculate ROI: Basic , Annualized and Monthly
+roi = ((revenue - investment) / investment) * 100
+annualized_roi = ((revenue / investment) ** (1/ investment_time) - 1) * 100
+monthly_roi = ((revenue / investment) ** (1/ investment_months) - 1) * 100
+
+gain_basic = revenue - investment
+gain_annualized = investment * (annualized_roi / 100) * investment_time
+gain_monthly = investment * (monthly_roi / 100) * investment_months
+
+#* Show results of gain and roi
+print(" ")
+print("*********************************************************")
+print(f" My first investment is: $ {investment:,.2f}")
+print(f" My time of investment is: {investment_time} years and {investment_months} months.")
+print(f" percentage of gain is: {roi:.2f}%")
+print(f" annualized percentage of gain is: {annualized_roi:.2f}%")
+print(f" monthly percentage of gain is: {monthly_roi:.2f}%")
+print(f" gain basic  is: ${gain_basic:,.2f}")
+print(f" gain annualized is: ${gain_annualized:,.2f} ")
+print(f" gain monthly is: ${gain_monthly:,.2f} ")
+print("*********************************************************")
+
+print(" 🌟🌟✨ 😉thank you for use this calculator of ROI.🌟🌟✨")
