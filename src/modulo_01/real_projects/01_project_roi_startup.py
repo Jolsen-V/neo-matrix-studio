2cd13ccfbbe3cb4619ff2ad955d1fe44b09dab2b
Jolsen-V <jvidalmilla03@gmail.com>
2026-05-17 18:20:42 -0500

    feat: modulo 01 - proyecto 01 calculadora ROI startup

--- src/modulo_01/real_projects/01_project_roi_startup.py
+++ src/modulo_01/real_projects/01_project_roi_startup.py
@@ -13,13 +13,14 @@ investment_time = int(input("How many years do you want to invest?:"))
 investment_months = investment_time * 12
 
 # calculate ROI: Basic , Annualized and Monthly
+gain_basic = revenue - investment
 roi = ((revenue - investment) / investment) * 100
 annualized_roi = ((revenue / investment) ** (1/ investment_time) - 1) * 100
-monthly_roi = (revenue / (investment * investment_months)) * 100
+monthly_roi = (gain_basic / (investment * investment_months)) * 100
 
-gain_basic = revenue - investment
 gain_annualized = investment * (annualized_roi / 100) * investment_time
 gain_monthly = investment * (monthly_roi / 100) * investment_months
+
 
 #* Show results of gain and roi
 print(" ")
